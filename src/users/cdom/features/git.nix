02f8853f1091623ad097dda50fd15be01cdcee6e
Chris Montgomery <chmont@protonmail.com>
2025-10-30 11:13:22 -0400

    git: add alias to reset permissions changes

--- src/users/cdom/features/git.nix
+++ src/users/cdom/features/git.nix
@@ -1,5 +1,13 @@
 {
   users.cdom.aspects.core.home = {
+    programs.git.aliases = {
+      # <https://stackoverflow.com/a/4408378/1801260>
+      "reset-permissions" = ''
+        !git diff -p -R --no-ext-diff --no-color --diff-filter=M \
+        | grep -E "^(diff|(old|new) mode)" --color=never \
+        | git apply
+      '';
+    };
     programs.git.extraConfig = {
       apply.whitespace = "nowarn";
       pull.rebase = true;
