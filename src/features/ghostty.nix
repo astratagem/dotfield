aad724c3729f590fd8b7d5cafbfdd069338d6968
chris montgomery <chmont@protonmail.com>
2026-01-02 16:05:03 -0500

    ghostty: bind shift+enter to enter newline

--- src/features/ghostty.nix
+++ src/features/ghostty.nix
@@ -5,6 +5,11 @@
       enableBashIntegration = true;
       enableFishIntegration = true;
       installBatSyntax = true;
+      settings = {
+        keybind = [
+          "shift+enter=text:\"\n\""
+        ];
+      };
     };
     dconf.settings."org/cinnamon/desktop/applications/terminal".exec = "ghostty";
   };
