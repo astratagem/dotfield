4166ee875b841b9e8a7681c121dabbe80b360059
chris montgomery <chmont@protonmail.com>
2026-01-14 04:16:26 -0500

    yubikey: improve support

--- src/features/hardware/by-manufacturer/yubico/yubikey.nix
+++ src/features/hardware/by-manufacturer/yubico/yubikey.nix
@@ -3,31 +3,18 @@
     nixos =
       { lib, pkgs, ... }:
       {
-        # using this requires use of `disable-ccid` in scdaemon.conf,
-        # and even still, it may be the cause of breaking pinentry for me 2024-05-05?
-        # <https://github.com/colemickens/nixcfg/blob/9baee703ddbba93250ef2feb74b25fb3625cbc12/mixins/gpg-agent.nix>
-        services.pcscd.enable = false;
-
+        services.pcscd.enable = true;
         hardware.gpgSmartcards.enable = true;
-
         services.udev.packages = [ pkgs.yubikey-personalization ];
-
         environment.systemPackages = [
           pkgs.age-plugin-yubikey
           pkgs.yubikey-personalization
-          # FIXME: broken on nixos-unstable 2024-05-05
-          # pkgs.yubikey-manager
+          pkgs.yubikey-manager
         ];
       };
 
-    home =
-      { pkgs, ... }:
-      {
-        home.packages = [
-          # XXX: build failure for dependency python3.12-pyscard-2.1.1
-          #  pkgs.yubikey-manager
-        ];
-        services.gpg-agent.enableScDaemon = true;
-      };
+    home = {
+      services.gpg-agent.enableScDaemon = true;
+    };
   };
 }
