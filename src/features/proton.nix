5a563aef3fdc8cb3fe9782c4f00eb2f85aa7ad7d
chris montgomery <chmont@protonmail.com>
2026-01-15 11:37:22 -0500

    proton: restore disabled packages

--- src/features/proton.nix
+++ src/features/proton.nix
@@ -2,10 +2,9 @@
   aspects.workstation.home =
     { pkgs, ... }:
     {
-      # XXX(2025-11-14): build failure in proton-core
-      # home.packages = with pkgs; [
-      #   protonmail-bridge-gui
-      #   protonvpn-gui
-      # ];
+      home.packages = with pkgs; [
+        protonmail-bridge-gui
+        protonvpn-gui
+      ];
     };
 }
