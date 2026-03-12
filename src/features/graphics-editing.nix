0f350b001842ea91cb90c9ca6872776efd302943
chris montgomery <chmont@protonmail.com>
2026-03-12 02:06:45 -0400

    feat(workstation): add various graphics editing tools

--- src/features/graphics-editing.nix
+++ src/features/graphics-editing.nix
@@ -3,8 +3,15 @@
     { pkgs, ... }:
     {
       home.packages = [
+        pkgs.darktable
+        pkgs.dia
+        pkgs.digikam
         pkgs.gimp-with-plugins
+        pkgs.gthumb
+        pkgs.hugin # image stitching (gui)
         pkgs.inkscape
+        pkgs.scribus
+        pkgs.shotwell
       ];
     };
 }
