df0f058c20e894830904b772a3890adda4b48786
chris montgomery <chmont@protonmail.com>
2026-05-12 13:47:35 -0400

    fix(dns): make custom nameservers opt-in

--- src/features/dns/quad9.nix
+++ src/features/dns/quad9.nix
@@ -3,7 +3,7 @@ let
   inherit (inputs.apparat.lib.net.constants) dns;
 in
 {
-  aspects.core.nixos = {
+  aspects.dns__quad9.nixos = {
     networking.nameservers = dns.nameservers.quad9;
     networking.networkmanager.insertNameservers = dns.nameservers.quad9;
   };
