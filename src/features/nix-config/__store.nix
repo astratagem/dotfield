77243a98c3441d09909f05e7e129311833836448
chris montgomery <chmont@protonmail.com>
2026-07-20 12:55:49 -0400

    nix-config: update settings based on srvos

--- src/features/nix-config/__store.nix
+++ src/features/nix-config/__store.nix
@@ -1,11 +1,16 @@
-{ lib, ... }: {
-  nix = {
-    ## `nix store optimise`
-    settings.auto-optimise-store = false;
-    optimise.automatic = true;
+{ config, lib, ... }: {
+  nix.settings.auto-optimise-store = false;
 
-    ## `nix store gc`
-    gc.dates = lib.mkDefault "weekly";
-    gc.automatic = lib.mkDefault true;
+  # De-duplicate store paths using hardlinks except in containers
+  # where the store is host-managed.
+  nix.optimise.automatic = lib.mkDefault (!config.boot.isContainer);
+
+  nix.gc.dates = lib.mkDefault "weekly";
+  nix.gc.automatic = lib.mkDefault true;
+
+  systemd.services.nix-gc.serviceConfig = {
+    CPUSchedulingPolicy = "batch";
+    IOSchedulingClass = "idle";
+    IOSchedulingPriority = 7;
   };
 }
--- src/features/nix-config/default.nix
+++ src/features/nix-config/default.nix
@@ -1,8 +1,7 @@
 # Copyright (C) 2022-2025 Chris Montgomery
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-flake@{ inputs, ... }:
-
+flake@{ lib, inputs, ... }:
 {
   aspects.core.nixos =
     {
@@ -15,33 +14,42 @@ flake@{ inputs, ... }:
     in
     {
       imports = [
+        ./__nix-daemon.nix
         ./__store.nix
         ./__substituters.nix
       ];
 
       environment.systemPackages = [ cfg.package ];
 
-      nix = {
-        nixPath = [
-          "nixpkgs=${pkgs.path}"
-          "home-manager=${inputs.home-manager}"
-        ];
-        distributedBuilds = true;
-        settings = {
-          allowed-users = [ "*" ];
-          trusted-users = [
-            "root"
-            "@wheel"
-          ];
+      # Disable nix channels. Use flakes instead.
+      nix.channel.enable = lib.mkDefault false;
+      nix.nixPath = [
+        "nixpkgs=${pkgs.path}"
+        "home-manager=${inputs.home-manager}"
+      ];
+      nix.distributedBuilds = true;
 
-          experimental-features = [
-            "flakes"
-            "nix-command"
-            (if cfg.package.pname == "lix" then "pipe-operator" else "pipe-operators")
-          ];
+      nix.settings.allowed-users = [ "*" ];
+      nix.settings.trusted-users = [
+        "root"
+        "@wheel"
+      ];
 
-          system-features = flake.config.meta.hosts.${config.networking.hostName}.supportedFeatures;
-        };
-      };
+      nix.settings.experimental-features = [
+        "flakes"
+        "nix-command"
+        (if cfg.package.pname == "lix" then "pipe-operator" else "pipe-operators")
+      ]
+      ++ lib.optional (lib.versionOlder (lib.versions.majorMinor config.nix.package.version) "2.22") "repl-flake";
+
+      nix.settings.system-features =
+        flake.config.meta.hosts.${config.networking.hostName}.supportedFeatures;
+
+      # The default at 10 is rarely enough.
+      nix.settings.log-lines = lib.mkDefault 25;
+
+      # Avoid disk full issues
+      nix.settings.max-free = lib.mkDefault (3000 * 1024 * 1024);
+      nix.settings.min-free = lib.mkDefault (512 * 1024 * 1024);
     };
 }
