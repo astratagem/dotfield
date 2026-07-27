1f5e5d9400bec0698dfc3a21dbdc5a7d31bc4b96
Chris Montgomery <chmont@protonmail.com>
2026-07-27 09:22:03 -0400

    feat(ghostty|theme): auto-generate 256-color palette from base16 colors

--- src/features/ghostty.nix
+++ src/features/ghostty.nix
@@ -7,6 +7,8 @@
       installBatSyntax = true;
       settings = {
         keybind = [ ];
+        # https://gist.github.com/jake-stewart/0a8ea46159a7da2c808e5be2177e1783
+        palette-generate = true;
       };
     };
     dconf.settings."org/cinnamon/desktop/applications/terminal".exec = "ghostty";
