f2360d5b0225ea2c925f0555751bac6bfaf24435
chris montgomery <chmont@protonmail.com>
2026-07-28 23:29:20 -0400

    refactor(shell|graphical): move color tools to a new feature

--- src/features/shell/default.nix
+++ src/features/shell/default.nix
@@ -39,13 +39,4 @@
         ];
       };
   };
-
-  aspects.graphical.home =
-    { pkgs, ... }:
-    {
-      home.packages = [
-        pkgs.colorpanes # <- print panes in the 8 bright terminal colors with shadows of the respective darker color
-        pkgs.pastel # <- generate, analyze, convert and manipulate colors
-      ];
-    };
 }
