92a24bc18eef7d57825877f36773eb75270159b4
chris montgomery <chmont@protonmail.com>
2026-07-31 13:38:28 -0400

    feat(libreoffice): provide linked Java Runtime Environment for extensions

--- src/features/libreoffice.nix
+++ src/features/libreoffice.nix
@@ -7,4 +7,12 @@
       pkgs.hunspellDicts.de-de
     ];
   };
+
+  aspects.workstation.home = { lib, pkgs, ... }: {
+    # Some LibreOffice extensions may require the Java Runtime
+    # Environment (JRE).  Unfortunately, this must be configured in the
+    # GUI settings with a stable path.
+    # <https://wiki.nixos.org/wiki/Zotero#Zotero_LibreOffice_add-on>
+    home.file.".local/share/java/jre8".source = pkgs.jre8;
+  };
 }
