1a1c5f4f1e889021605a97b72dbd9c443e8d0b80
chris montgomery <chmont@protonmail.com>
2026-09-18 10:33:45 -0400

    tldr: delete removed setting `updateOnActivation`

--- src/features/tldr.nix
+++ src/features/tldr.nix
@@ -16,13 +16,6 @@
           use_pager = false;
           compact = false;
         };
-        # Prevent activation failure during boot when using the NixOS module and
-        # network is not yet available. Even if network were available, updating
-        # this cache during boot is not appropriate.
-        #
-        # Note that this option will likely be removed in the near future:
-        # <https://github.com/nix-community/home-manager/pull/5005>
-        updateOnActivation = lib.mkForce false;
         updates = {
           auto_update = true;
           auto_update_interval_hours = 24 * 7;
